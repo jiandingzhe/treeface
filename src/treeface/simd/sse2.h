4145967a7280e743ffaec8a42a3b16a8af1a11c7
Xi Yang <yangx@im.ac.cn>
2015-03-17 12:52:39 +0800

    fixed bug on SIMD sum added more tests on SIMD arithmetics

--- src/treeface/simd/sse2.h
+++ src/treeface/simd/sse2.h
@@ -139,19 +139,10 @@ SIMDType<16> simd_cmp<float, 16> (SIMDType<16> a, SIMDType<16>  b)
 template<>
 float simd_sum<float, 16>(SIMDType<16> a)
 {
-    float temp;
-    SIMDType<16> b = a;
-    SIMDType<16> c;
-    SIMDType<16> d;
-    SIMDType<16> e;
-    c.simd_by_int = _mm_srli_epi32(a.simd_by_int, 32);
-    d.simd_by_int = _mm_srli_epi32(a.simd_by_int, 64);
-    b.simd_by_float = _mm_add_ss(b.simd_by_float, c.simd_by_float);
-    b.simd_by_float = _mm_add_ss(b.simd_by_float, d.simd_by_float);
-    e.simd_by_int = _mm_srli_epi32(a.simd_by_int, 96);
-    b.simd_by_float = _mm_add_ss(b.simd_by_float, e.simd_by_float);
-    _mm_store_ss(&temp, b.simd_by_float);
-    return temp;
+    return a.values_by_float[0] +
+            a.values_by_float[1] +
+            a.values_by_float[2] +
+            a.values_by_float[3];
 }
 
 TREEFACE_NAMESPACE_END
--- t/t_simd.cpp
+++ t/t_simd.cpp
@@ -5,21 +5,58 @@ using namespace treeface;
 
 void TestFramework::content()
 {
+    OK("set");
     SIMDType<16> a = simd_set<float, 16>(1, 2, 3, 4);
     IS(a.values_by_float[0], 1);
     IS(a.values_by_float[1], 2);
     IS(a.values_by_float[2], 3);
     IS(a.values_by_float[3], 4);
 
+    OK("binary layout");
+    float v0 = simd_get_one<0, float>(a);
+    float v1 = simd_get_one<1, float>(a);
+    float v2 = simd_get_one<2, float>(a);
+    float v3 = simd_get_one<3, float>(a);
+    IS(v0, a.values_by_float[0]);
+    IS(v1, a.values_by_float[1]);
+    IS(v2, a.values_by_float[2]);
+    IS(v3, a.values_by_float[3]);
+
+    OK("sum");
+    float sum = simd_sum<float>(a);
+    IS(sum, v0 + v1 + v2 + v3);
+
+    OK("add");
     SIMDType<16> b = simd_set<float, 16>(5, 6, 7, 8);
     IS(b.values_by_float[0], 5);
     IS(b.values_by_float[1], 6);
     IS(b.values_by_float[2], 7);
     IS(b.values_by_float[3], 8);
 
-    SIMDType<16> c = simd_add<float, 16>(a, b);
+    SIMDType<16> c = simd_add<float>(a, b);
     IS(c.values_by_float[0], 6);
     IS(c.values_by_float[1], 8);
     IS(c.values_by_float[2], 10);
     IS(c.values_by_float[3], 12);
+
+    OK("sub");
+    c = simd_sub<float>(a, b);
+    IS(c.values_by_float[0], -4);
+    IS(c.values_by_float[1], -4);
+    IS(c.values_by_float[2], -4);
+    IS(c.values_by_float[3], -4);
+
+    OK("mul");
+    c = simd_mul<float>(a, b);
+    IS(c.values_by_float[0], 5);
+    IS(c.values_by_float[1], 12);
+    IS(c.values_by_float[2], 21);
+    IS(c.values_by_float[3], 32);
+
+    OK("div");
+    c = simd_div<float>(a, b);
+    IS(c.values_by_float[0], float(1)/5);
+    IS(c.values_by_float[1], float(2)/6);
+    IS(c.values_by_float[2], float(3)/7);
+    IS(c.values_by_float[3], float(4)/8);
 }
