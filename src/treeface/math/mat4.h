93d377586e494948cddaae32a05c4af32971fa2e
Xi Yang <yangx@im.ac.cn>
2015-03-17 08:59:19 +0800

    implement using SIMD types

--- src/treeface/math/mat4.h
+++ src/treeface/math/mat4.h
@@ -1,20 +1,23 @@
 #ifndef TREEFACE_MAT4_H
 #define TREEFACE_MAT4_H
 
-#include "treeface/common.h"
+#include "treeface/simd.h"
 
 TREEFACE_NAMESPACE_BEGIN
 
-template<typename T>
+template<typename T, int SZ = sizeof(T) * 4>
 struct Mat4
 {
-    Mat4() {}
+    typedef SIMDType<SZ> DataType;
+    Mat4()
+    {
+        data[0] = simd_set<T, SZ>(1, 0, 0, 0);
+        data[1] = simd_set<T, SZ>(0, 1, 0, 0);
+        data[2] = simd_set<T, SZ>(0, 0, 1, 0);
+        data[4] = simd_set<T, SZ>(0, 0, 0, 1);
+    }
 
-
-    float data[4][4] = {{1, 0, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 1}};
+    DataType data[4];
 };
 
 TREEFACE_NAMESPACE_END
