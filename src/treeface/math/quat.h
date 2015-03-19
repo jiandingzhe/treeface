0ce8acec98b23b245cbeaa6a45b1673cb8c630ba
Xi Yang <yangx@im.ac.cn>
2015-03-19 16:33:08 +0800

    added some documentation

--- src/treeface/math/quat.h
+++ src/treeface/math/quat.h
@@ -5,15 +5,39 @@
 
 TREEFACE_NAMESPACE_BEGIN
 
+/**
+ * Quaternions are frequently used in 3D rotation.
+ */
 template<typename T, int SZ = sizeof(T) * 4>
 struct Quat
 {
     typedef SIMDType<SZ> DataType;
 
+    /**
+     * @brief create quaternion representing zero rotation
+     */
     Quat(): data(simd_set<T, SZ>(0, 0, 0, 1)) {}
+
+    /**
+     * @brief crete quaternion with value specified
+     * @param value
+     */
     Quat(SIMDType<SZ> value): data(value) {}
+
+    /**
+     * @brief create quaternion by specifying all four components
+     * @param x: component 1
+     * @param y: component 2
+     * @param z: component 3
+     * @param w: component 4, represents cos(a/2)
+     */
     Quat(T x, T y, T z, T w): data(simd_set<T, SZ>(x, y, z, w)) {}
 
+    /**
+     * @brief create quaternion with rotation specified by right-hand angle and axis
+     * @param angle: rotation around axis
+     * @param axis: direction of rotaton axis. The 4rd component will be omitted
+     */
     Quat(T angle, Vec4<T> axis)
     {
         set_angle_axis(angle, axis);
@@ -55,6 +79,11 @@ struct Quat
         return simd_get_one<3, float>(data);
     }
 
+    /**
+     * @brief get rotation represented by this quaternion
+     * @param angle: variable for storing angle
+     * @param axis: variable for storing axis in (x, y, z, 0)
+     */
     void get_angle_axis(T& angle, Vec4<T>& axis) const noexcept
     {
         T cos_value = simd_get_one<3, float>(data);
@@ -102,6 +131,11 @@ struct Quat
         data = simd_set_one<3>(data, value);
     }
 
+    /**
+     * @brief set rotation by specifying right-hand angle and axis
+     * @param angle: rotation around axis
+     * @param axis: direction of rotation axis. The 4rd component will be omitted.
+     */
     void set_angle_axis(T angle, Vec4<T> axis) noexcept
     {
         axis.set_w(0);
@@ -113,8 +147,16 @@ struct Quat
         data = simd_set_one<3, T, SZ>(data, std::cos(angle / 2));
     }
 
+    /**
+     * @brief get quaternion (-x, -y, -z, w)
+     * @return inversed quaternion
+     */
     Quat<T> inverse() const noexcept;
 
+    /**
+     * @brief make quaternion length to be one
+     * @return length before normalize
+     */
     T normalize() noexcept
     {
         T len = length();
@@ -122,16 +164,30 @@ struct Quat
         return len;
     }
 
+    /**
+     * @brief get quaternion length
+     * @return length value
+     */
     T length() const noexcept
     {
         return std::sqrt(length2());
     }
 
+    /**
+     * @brief get length square
+     * @return length * length
+     */
     T length2() const noexcept
     {
         return simd_sum<T>(simd_mul<T>(data, data));
     }
 
+    /**
+     * @brief apply rotation represented by this quaternion to a vector
+     * @param input: vector to be rotated. To get correct result, the 4rd
+     *        component of input vector should be zero.
+     * @return rotated vector
+     */
     Vec4<T> rotate(Vec4<T> input) const noexcept
     {
         Quat<T> re =  *this * Quat<T>(input.data) * inverse();
@@ -148,7 +204,9 @@ Quat<float> Quat<float>::inverse() const noexcept
     return Quat<float>(simd_xor<float>(data, simd_set<std::int32_t, 16>(0x80000000, 0x80000000, 0x80000000, 0x00000000)));
 }
 
-
+/**
+ * @brief quaternion multiply
+ */
 template<typename T, int SZ = sizeof(T) * 4>
 Quat<T> operator * (Quat<T> a, Quat<T> b) noexcept
 {
