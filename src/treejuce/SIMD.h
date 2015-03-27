bfe91e4b2c57b554c885d3f18c9ee13336472fa8
Xi Yang <yangx@im.ac.cn>
2015-03-27 09:40:30 +0800

    empty implementation of ARM neon SIMD instructions.

--- src/treejuce/SIMD.h
+++ src/treejuce/SIMD.h
@@ -1,10 +1,12 @@
 #ifndef TREEJUCE_SIMD_FUNC_H
 #define TREEJUCE_SIMD_FUNC_H
 
-#include "treeface/config.h"
+#include "treejuce/TargetPlatform.h"
 
-#if defined TREEFACE_CPU_X86_64
+#if defined JUCE_INTEL
 #  include "treejuce/SIMD/sse2.h"
+#elif defined JUCE_ARM
+#  include "treejuce/SIMD/neon.h"
 #else
 #  error "SIMD function wrapper for CPU is not implemented"
 #endif
---
+++ src/treejuce/SIMD/neon.h
@@ -0,0 +1,12 @@
+#ifndef TREEJUCE_SIMD_NEON_H
+#define TREEJUCE_SIMD_NEON_H
+
+#include "treejuce/Common.h"
+
+TREEFACE_JUCE_NAMESPACE_BEGIN
+
+#error "ARM neon not implemented"
+
+TREEFACE_JUCE_NAMESPACE_END
+
+#endif // TREEJUCE_SIMD_NEON_H
