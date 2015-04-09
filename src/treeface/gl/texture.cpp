7eb94de1975f24cbd51c7e2461f6c9ac3276a946
Xi Yang <yangx@im.ac.cn>
2015-04-09 17:00:52 +0800

    Added texture parameters. Automatically set mipmap level range while set texture image.

--- src/treeface/gl/texture.cpp
+++ src/treeface/gl/texture.cpp
@@ -1,3 +1,4 @@
+#include "treeface/gl/imageref.h"
 #include "treeface/gl/texture.h"
 
 #include <treejuce/String.h>
@@ -59,27 +60,114 @@ void Texture::init() NOEXCEPT
         die("failed to generate texture");
 }
 
-void Texture::set_image_data(GLint internal_fmt,
-                             GLenum format, GLenum type,
-                             GLsizei w, GLsizei h, treejuce::uint8* data) NOEXCEPT
+treejuce::Result Texture::set_image_data(ImageRef image, GLint internal_fmt) NOEXCEPT
 {
     glBindTexture(GL_TEXTURE_2D, m_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, internal_fmt, w, h, 0, format, type, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 internal_fmt,
+                 image.width,
+                 image.height,
+                 0,
+                 image.format,
+                 image.type,
+                 image.data);
 
     GLenum err = glGetError();
-    if (err != GL_NO_ERROR)
-        die("set_image_data failed with %d\n", err);
-
     glBindTexture(GL_TEXTURE_2D, 0);
+
+    if (err == GL_NO_ERROR)
+    {
+        return treejuce::Result::ok();
+    }
+    else
+    {
+        switch(err)
+        {
+        case GL_INVALID_ENUM:
+            return treejuce::Result::fail("texture failed with \"invalid enum\"");
+        case GL_INVALID_VALUE:
+            return treejuce::Result::fail("texture failed with \"invalid value\"");
+        case GL_INVALID_OPERATION:
+            return treejuce::Result::fail("texture failed with \"invalid operation\"");
+        default:
+            return treejuce::Result::fail("texture failed with other error type: "+String(err));
+        }
+    }
 }
 
-void Texture::set_mipmap_data(GLint internal_fmt,
-                              GLenum format, GLenum type,
-                              GLsizei w, GLsizei h, treejuce::uint8* data) NOEXCEPT
+treejuce::Result Texture::set_image_data(treejuce::ArrayRef<ImageRef> images, GLint internal_fmt) NOEXCEPT
 {
     glBindTexture(GL_TEXTURE_2D, m_texture);
-    gluBuild2DMipmaps(GL_TEXTURE_2D, internal_fmt, w, h, format, type, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, images.size() - 1);
+
+    for (int level = 0; level < images.size(); level++)
+    {
+        glTexImage2D(GL_TEXTURE_2D,
+                     level,
+                     internal_fmt,
+                     images[level].width,
+                     images[level].height,
+                     0,
+                     images[level].format,
+                     images[level].type,
+                     images[level].data);
+
+        GLenum err = glGetError();
+
+        if (err != GL_NO_ERROR)
+        {
+            glBindTexture(GL_TEXTURE_2D, 0);
+            switch(err)
+            {
+            case GL_INVALID_ENUM:
+                return treejuce::Result::fail("texture failed with \"invalid enum\" while setting mipmap level "+String(level));
+            case GL_INVALID_VALUE:
+                return treejuce::Result::fail("texture failed with \"invalid value\" while setting mipmap level "+String(level));
+            case GL_INVALID_OPERATION:
+                return treejuce::Result::fail("texture failed with \"invalid operation\" while setting mipmap level "+String(level));
+            default:
+                return treejuce::Result::fail("texture failed with other error type: "+String(err)+" while setting mipmap level "+String(level));
+            }
+        }
+    }
+
     glBindTexture(GL_TEXTURE_2D, 0);
+    return treejuce::Result::ok();
+}
+
+treejuce::Result Texture::set_mipmap_data(ImageRef image, GLint internal_fmt) NOEXCEPT
+{
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, internal_fmt, image.width, image.height, image.format, image.type, image.data);
+
+    GLenum err = glGetError();
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    if (err == GL_NO_ERROR)
+    {
+        return treejuce::Result::ok();
+    }
+    else
+    {
+        switch(err)
+        {
+        case GL_INVALID_ENUM:
+            return treejuce::Result::fail("texture failed with \"invalid enum\"");
+        case GL_INVALID_VALUE:
+            return treejuce::Result::fail("texture failed with \"invalid value\"");
+        case GL_INVALID_OPERATION:
+            return treejuce::Result::fail("texture failed with \"invalid operation\"");
+        default:
+            return treejuce::Result::fail("texture failed with other error type: "+String(err));
+        }
+    }
 }
 
 TREEFACE_NAMESPACE_END
--- src/treeface/gl/texture.h
+++ src/treeface/gl/texture.h
@@ -3,37 +3,136 @@
 
 #include "treeface/common.h"
 
-#include <treejuce/Array.h>
+#include <treejuce/ArrayRef.h>
 #include <treejuce/MathsFunctions.h>
+#include <treejuce/Object.h>
+#include <treejuce/Result.h>
 
 #include <GL/glew.h>
 
 TREEFACE_NAMESPACE_BEGIN
+
+class ImageRef;
 
 extern const GLenum TEXTURE_UNITS[32];
 
 /**
  * @brief wrapper for OpenGL 2D texture
  */
-struct Texture
+class Texture: public treejuce::Object
 {
+public:
     Texture();
-
     ~Texture();
+
+    // disable copy and move
+    Texture(const Texture&) = delete;
+    Texture(Texture&&) = delete;
+    Texture& operator = (const Texture&) = delete;
+    Texture& operator = (Texture&&) = delete;
 
     void init() NOEXCEPT;
 
-    void set_image_data(GLint internal_fmt,
-                        GLenum format, GLenum type,
-                        GLsizei w, GLsizei h, treejuce::uint8* data) NOEXCEPT;
+    /**
+     * Set image data and set to only using single mipmap level.
+     *
+     * Mipmap base level and max level will be automatically set to zero.
+     *
+     * @param image: the image to be set
+     * @param internal_fmt: device-side image format
+     */
+    treejuce::Result set_image_data(ImageRef image, GLint internal_fmt) NOEXCEPT;
 
-    void set_mipmap_data(GLint internal_fmt,
-                         GLenum format, GLenum type,
-                         GLsizei w, GLsizei h, treejuce::uint8* data) NOEXCEPT;
+    /**
+     * Specify image data for all mipmap levels.
+     *
+     * Mipmap base level and max level will be automatically adjusted. But you
+     * still need to manually modify filter_min to those mipmap-enabled modes.
+     *
+     * @param images: images to be set for all mipmap levels
+     * @param internal_fmt: device-side image format
+     */
+    treejuce::Result set_image_data(treejuce::ArrayRef<ImageRef> images, GLint internal_fmt) NOEXCEPT;
 
-    inline void use() const NOEXCEPT
+    /**
+     * automatically generate all mipmap levels using one image data.
+     *
+     * Mipmap base level and max level will be automatically adjusted. But you
+     * still need to manually modify filter_min to those mipmap-enabled modes.
+     *
+     * @param image: the image to be set
+     * @param internal_fmt: device-side image format
+     */
+    treejuce::Result set_mipmap_data(ImageRef image, GLint internal_fmt) NOEXCEPT;
+
+    inline void set_min_lod(float value) NOEXCEPT
+    {
+        if (m_min_lod != value)
+        {
+            m_param_changed = true;
+            m_min_lod = value;
+        }
+    }
+
+    inline void set_max_lod(float value) NOEXCEPT
+    {
+        if (m_max_lod != value)
+        {
+            m_param_changed = true;
+            m_max_lod = value;
+        }
+    }
+
+    inline void set_wrap_s(GLenum value) NOEXCEPT
+    {
+        if (m_wrap_s != value)
+        {
+            m_param_changed = true;
+            m_wrap_s = value;
+        }
+    }
+
+    inline void set_wrap_t(GLenum value) NOEXCEPT
+    {
+        if (m_wrap_t != value)
+        {
+            m_param_changed = true;
+            m_wrap_t = value;
+        }
+    }
+
+    inline void set_min_filter(GLenum value) NOEXCEPT
+    {
+        if (m_min_filter != value)
+        {
+            m_param_changed = true;
+            m_min_filter = value;
+        }
+    }
+
+    inline void set_mag_filter(GLenum value) NOEXCEPT
+    {
+        if (m_mag_filter != value)
+        {
+            m_param_changed = true;
+            m_mag_filter = value;
+        }
+    }
+
+    inline void use() NOEXCEPT
     {
         glBindTexture(GL_TEXTURE_2D, m_texture);
+
+        if (m_param_changed)
+        {
+            m_param_changed = false;
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_LOD, m_min_lod);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_LOD, m_max_lod);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_wrap_s);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_wrap_t);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_min_filter);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_mag_filter);
+        }
     }
 
     inline void unuse() const NOEXCEPT
@@ -41,7 +140,19 @@ struct Texture
         glBindTexture(GL_TEXTURE_2D, 0);
     }
 
+protected:
     GLuint m_texture;
+
+    // texture parameters
+    // If modified, will be uploaded to device on call to use().
+
+    bool m_param_changed = true;
+    float m_min_lod = -1000;
+    float m_max_lod = 1000;
+    GLenum m_wrap_s = GL_REPEAT;
+    GLenum m_wrap_t = GL_REPEAT;
+    GLenum m_min_filter = GL_LINEAR;
+    GLenum m_mag_filter = GL_LINEAR;
 };
 
 TREEFACE_NAMESPACE_END
