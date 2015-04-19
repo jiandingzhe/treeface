20abdb11b9bffc023ab2f6992fe4ec1758da41be
Xi Yang <yangx@im.ac.cn>
2015-04-19 14:41:51 +0800

    Use juce Logger to provide output, instead of using stream in c++ standard library.

--- t/TestFramework.cpp
+++ t/TestFramework.cpp
@@ -1,10 +1,10 @@
 #include "TestFramework.h"
 
-#include <iostream>
-
+#include <treejuce/Logger.h>
 #include <treejuce/Result.h>
+#include <treejuce/String.h>
 
-using namespace std;
+using namespace treejuce;
 
 TestFramework::TestFramework(): n_planned(0), n_got(0), n_fail(0)
 {
@@ -14,7 +14,7 @@ TestFramework::TestFramework(size_t n_tests): n_planned(n_tests), n_got(0), n_fa
 {
     if (!n_tests)
     {
-        cerr << "zero number of planned tests" << endl;
+        Logger::writeToLog("zero number of planned tests");
         exit(EXIT_FAILURE);
     }
 }
@@ -28,12 +28,12 @@ bool TestFramework::run()
     content();
     if (n_planned && n_planned != n_got)
     {
-        cerr << "planned " << n_planned << ", but only run " << n_got << endl;
+        Logger::writeToLog("planned " + String(n_planned) + ", but only run " + String(n_got));
         return false;
     }
     else
     {
-        cout << n_got << " tests, " << n_fail << " failed" << endl;
+        Logger::writeToLog(String(n_got) + " tests, " + String(n_fail) + " failed");
         return (n_fail == 0);
     }
 }
@@ -43,11 +43,11 @@ void TestFramework::ok(bool value, const char *desc)
     n_got++;
     if (value)
     {
-        cout << "ok " << n_got << " - " << desc << endl;
+        Logger::writeToLog("ok " + String(n_got) + " - " + desc);
     }
     else
     {
-        cout << "NOT ok " << n_got << " - " << desc << endl;
+        Logger::writeToLog("NOT ok " + String(n_got) + " - " + desc);
         n_fail++;
     }
 }
@@ -57,11 +57,11 @@ void TestFramework::ok(const treejuce::Result& result, const char* desc)
     n_got++;
     if (result)
     {
-        cout << "ok " << n_got << " - " << desc << endl;
+        Logger::writeToLog("ok " + String(n_got) + " - " + desc);
     }
     else
     {
-        cout << "NOT ok " << n_got << " - " << result.getErrorMessage().toRawUTF8() << endl;
+        Logger::writeToLog("NOT ok " + String(n_got) + " - " + result.getErrorMessage());
         n_fail++;
     }
 }
