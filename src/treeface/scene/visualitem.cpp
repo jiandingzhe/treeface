ac929a454e6de2eb5dff98ce3783eb340af79b1b
Xi Yang <yangx@im.ac.cn>
2015-05-03 19:42:47 +0800

    Implemented VisualItem. Not tested yet.

--- src/treeface/scene/visualitem.cpp
+++ src/treeface/scene/visualitem.cpp
@@ -1,7 +1,110 @@
 #include "treeface/scene/visualitem.h"
+#include "treeface/scene/material.h"
+#include "treeface/scene/geometry.h"
+#include "treeface/gl/vertexarray.h"
+
+#include "treeface/geometrymanager.h"
+#include "treeface/materialmanager.h"
+
+#include "treeface/misc/propertyvalidator.h"
+
+#include <treejuce/DynamicObject.h>
+#include <treejuce/Holder.h>
+#include <treejuce/NamedValueSet.h>
+#include <treejuce/Result.h>
+#include <treejuce/Variant.h>
+
+using namespace treejuce;
 
 TREEFACE_NAMESPACE_BEGIN
 
+struct VisualItem::Impl
+{
+    Holder<Material> material = nullptr;
+    Holder<Geometry> geometry = nullptr;
+    Holder<VertexArray> vertex_array = nullptr;
+};
 
+VisualItem::VisualItem(): m_impl(new Impl())
+{
+}
+
+VisualItem::~VisualItem()
+{
+    if (m_impl)
+        delete m_impl;
+}
+
+treejuce::Result VisualItem::build(const treejuce::String& geom_name, const treejuce::String& mat_name) NOEXCEPT
+{
+    Geometry* geom = nullptr;
+    Result re_geom = GeometryManager::getInstance()->get_geometry(geom_name, &geom);
+    if (!re_geom)
+        return re_geom;
+
+    Material* mat = nullptr;
+    Result re_mat = MaterialManager::getInstance()->get_material(mat_name, &mat);
+    if (!re_mat)
+        return re_mat;
+
+    return build(geom, mat);
+}
+
+#define KEY_MAT "material"
+#define KEY_GEO "geometry"
+Result _validate_(const treejuce::NamedValueSet& kv)
+{
+    static PropertyValidator* validator = nullptr;
+    if (!validator)
+    {
+        validator = new PropertyValidator();
+        validator->add_item(KEY_MAT, PropertyValidator::ITEM_SCALAR, true);
+        validator->add_item(KEY_GEO, PropertyValidator::ITEM_SCALAR, true);
+    }
+
+    return validator->validate(kv);
+}
+
+treejuce::Result VisualItem::build(const treejuce::var& node) NOEXCEPT
+{
+    if (!node.isObject())
+        return Result::fail("visual item node is not KV");
+
+    const NamedValueSet& node_kv = node.getDynamicObject()->getProperties();
+    {
+        Result re = _validate_(node_kv);
+        if (!re)
+            return re;
+    }
+
+    String name_mat = node_kv[KEY_MAT].toString();
+    String name_geo = node_kv[KEY_GEO].toString();
+
+    return build(name_geo, name_mat);
+}
+
+treejuce::Result VisualItem::build(Geometry* geom, Material* mat) NOEXCEPT
+{
+    m_impl->geometry = geom;
+    m_impl->material = mat;
+    m_impl->vertex_array = new VertexArray();
+
+    return m_impl->vertex_array->build(geom->get_buffer(), geom->get_vertex_template(), mat->get_program());
+}
+
+Material* VisualItem::get_material() NOEXCEPT
+{
+    return m_impl->material;
+}
+
+Geometry* VisualItem::get_geometry() NOEXCEPT
+{
+    return m_impl->geometry;
+}
+
+VertexArray* VisualItem::get_vertex_array() NOEXCEPT
+{
+    return m_impl->vertex_array;
+}
 
 TREEFACE_NAMESPACE_END
--- src/treeface/scene/visualitem.h
+++ src/treeface/scene/visualitem.h
@@ -5,7 +5,16 @@
 
 #include <treejuce/Object.h>
 
+TREEFACE_JUCE_NAMESPACE_BEGIN
+class Result;
+class var;
+TREEFACE_JUCE_NAMESPACE_END
+
 TREEFACE_NAMESPACE_BEGIN
+
+class Material;
+class Geometry;
+class VertexArray;
 
 class VisualItem: public treejuce::Object
 {
@@ -13,7 +22,27 @@ public:
     VisualItem();
     virtual ~VisualItem();
 
-    virtual void draw() = 0;
+    VisualItem(VisualItem&& other): m_impl(other.m_impl)
+    {
+        other.m_impl = nullptr;
+    }
+
+    VisualItem& operator = (VisualItem&& other)
+    {
+        jassert(m_impl == nullptr);
+        m_impl = other.m_impl;
+        other.m_impl = nullptr;
+    }
+
+    JUCE_DECLARE_NON_COPYABLE(VisualItem);
+
+    treejuce::Result build(const treejuce::String& geom_name, const treejuce::String& mat_name) NOEXCEPT;
+    treejuce::Result build(const treejuce::var& node) NOEXCEPT;
+    treejuce::Result build(Geometry* geom, Material* mat) NOEXCEPT;
+
+    Material* get_material() NOEXCEPT;
+    Geometry* get_geometry() NOEXCEPT;
+    VertexArray* get_vertex_array() NOEXCEPT;
 
 private:
     struct Impl;
