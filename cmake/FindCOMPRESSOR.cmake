d5fc9271ad4a49b3a09c5ed6558a1c6d74970926
Xi Yang <jiandingzhe@163.com>
2015-05-12 14:53:40 +0800

    changed abstract target naming
    Use output file base name instead of replacing full path by "___", as
    full path may contain comma in volume part, which is invalid to be used
    as a abstract target name.

--- cmake/FindCOMPRESSOR.cmake
+++ cmake/FindCOMPRESSOR.cmake
@@ -10,20 +10,21 @@ find_program(COMPRESSOR_EXECUTABLE
 
 #
 # usage: ZIP_FILES(output_file input [input2 input3 ...])
+# the output file will be created at cmake_current_binary_dir
 #
 function(ZIP_FILES OUTPUT_FILE)
     foreach(curr_file ${ARGN})
         if(IS_ABSOLUTE ${curr_file})
-            list(APPEND input_files ${curr_file})
+            list(APPEND input_files_full ${curr_file})
         else()
-            list(APPEND input_files ${CMAKE_CURRENT_SOURCE_DIR}/${curr_file})
+            list(APPEND input_files_full ${CMAKE_CURRENT_SOURCE_DIR}/${curr_file})
         endif()
     endforeach(curr_file)
 
     if(IS_ABSOLUTE ${OUTPUT_FILE})
-        set(output_file_use ${OUTPUT_FILE})
+        set(output_file_full ${OUTPUT_FILE})
     else()
-        set(output_file_use ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_FILE})
+        set(output_file_full ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_FILE})
     endif()
 
     get_filename_component(compressor_basename ${COMPRESSOR_EXECUTABLE} NAME_WE)
@@ -31,27 +32,26 @@ function(ZIP_FILES OUTPUT_FILE)
     # create zip archive using 7-zip
     if(compressor_basename STREQUAL 7z OR compressor_basename STREQUAL 7za)
         add_custom_command(
-            DEPENDS ${input_files}
-            OUTPUT ${output_file_use}
-            COMMAND ${COMPRESSOR_EXECUTABLE} a -tzip ${output_file_use} ${ARGN}
+            DEPENDS ${input_files_full}
+            OUTPUT ${output_file_full}
+            COMMAND ${COMPRESSOR_EXECUTABLE} a -tzip ${output_file_full} ${ARGN}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     # create zip archive using zip
     elseif(compressor_basename STREQUAL zip)
         add_custom_command(
-            DEPENDS ${input_files}
-            OUTPUT ${output_file_use}
-            COMMAND ${COMPRESSOR_EXECUTABLE} ${output_file_use} ${ARGN}
+            DEPENDS ${input_files_full}
+            OUTPUT ${output_file_full}
+            COMMAND ${COMPRESSOR_EXECUTABLE} ${output_file_full} ${ARGN}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     else()
         message(FATAL_ERROR "invalid compressor program base name: ${compressor_basename}")
     endif()
 
-    string(REPLACE "/" "___" zip_target ${output_file_use})
-    add_custom_target(ZIP_TARGET_${zip_target} ALL
-        DEPENDS ${output_file_use}
-        SOURCES ${input_files}
+    add_custom_target(${OUTPUT_FILE} ALL
+        DEPENDS ${output_file_full}
+        SOURCES ${input_files_full}
     )
 endfunction()
 
