f091590e2182b689732daf39c962db7dde4e9ffe
Xi Yang <yangx@im.ac.cn>
2015-05-13 15:09:01 +0800

    try for not rendering issue

--- test/interact_wrap.cpp
+++ test/interact_wrap.cpp
@@ -426,7 +426,8 @@ int main(int argc, char** argv)
         renderer.render();
 
         SDL_GL_SwapWindow(window);
-        SDL_Delay(20);
+        SDL_Delay(500);
+        break;
     }
 
     SDL_GL_DeleteContext(context);
